ff032b2f9e15ac6109525ea05b5ec9432cfad6f4
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-01-31 18:21:44 +0700

    Add refresh token migration

--- migrations/20210131104949-create-table-refresh-tokens.js
+++ migrations/20210131104949-create-table-refresh-tokens.js
@@ -2,20 +2,33 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+     await queryInterface.createTable('refresh_tokens', {
+       id: {
+         type: Sequelize.INTEGER,
+         primaryKey: true,
+         autoIncrement: true,
+         allowNull: false
+       },
+       token: {
+         type: Sequelize.TEXT,
+         allowNull: false,
+       },
+       user_id: {
+         type: Sequelize.INTEGER,
+         allowNull: false,
+       },
+       created_at: {
+         type: Sequelize.DATE,
+         allowNull: false
+       },
+       updated_at: {
+         type: Sequelize.DATE,
+         allowNull: false
+       }
+     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+     await queryInterface.dropTable('refresh_tokens');
   }
 };
