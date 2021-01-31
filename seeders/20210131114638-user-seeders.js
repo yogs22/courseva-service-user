910eafc0dc563a8c30c070253620057525ea5fe7
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-01-31 18:53:01 +0700

    Add users seeder

---
+++ seeders/20210131114638-user-seeders.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Yogi',
+        profession: 'Admin Micro',
+        role: 'admin',
+        email: 'yogi@gmail.com',
+        password: await bcrypt.hash('password', 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Yasa',
+        profession: 'Front End Developer',
+        role: 'student',
+        email: 'yasa@gmail.com',
+        password: await bcrypt.hash('password', 10),
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+     await queryInterface.bulkDelete('users', null, {});
+  }
+};
