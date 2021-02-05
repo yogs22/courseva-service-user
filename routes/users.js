9e5be6a6ce5a3451a184c072d3384cea94ad6ae5
Yogi Prasetyawan <yogi.enjoyers22@gmail.com>
2021-02-06 06:06:50 +0700

    Add find by id user

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const usersHandler = require('./handler/users');
 
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
-router.put('/:id', usersHandler.update)
+router.put('/:id', usersHandler.update);
+router.get('/:id', usersHandler.getUser);
 
 module.exports = router;
